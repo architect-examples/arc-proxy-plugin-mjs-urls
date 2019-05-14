c47f3d8e20e47fe6b352356d92cbe7c382aaef2f
Brian LeRoux <b@brian.io>
2019-05-14 10:43:18 -0700

    updates to new base64 capabilities

--- index.js
+++ index.js
@@ -15,17 +15,18 @@ let escodegen = require('escodegen')
  * @param File - the file contents {headers, body}
  * @returns File - the processed file contents {header, body}
  */
-module.exports = function mjs(Key, {headers, body}) {
+module.exports = function mjs(Key, {headers, body, isBase64Encoded}) {
   let prefixing = process.env.NODE_ENV != 'testing'
   if (prefixing) {
-    let ast = esprima.parseModule(body, {}, function visit(node) {
+    let raw = isBase64Encoded? Buffer.from(body, 'base64').toString() : body
+    let ast = esprima.parseModule(raw, {}, function visit(node) {
       if (node.type === 'ImportDeclaration') {
         let val = `/${process.env.NODE_ENV}${node.source.value}`
         node.source.value = val
       }
       return node
     })
-    return {headers, body: escodegen.generate(ast)}
+    return {headers, body: escodegen.generate(ast), isBase64Encoded:!!(isBase64Encoded)}
   }
   else {
     return {headers, body}
